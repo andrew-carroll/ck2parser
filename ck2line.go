a6904b913e6d1009a2f19779aa45f363361dba7d
Andrew Carroll <mrandrewcarroll@gmail.com>
2017-08-11 14:34:46 -0400

    Determines property types

--- ck2line.go
+++ ck2line.go
@@ -5,34 +5,44 @@ import (
 )
 
 type CK2Line struct {
-	rawLine string
-	pattern Pattern
-	name    string
-	value   string
+	rawLine      string
+	pattern      pattern
+	name         string
+	value        string
+	propertyType propertyType
 }
 
-type Pattern string
+type pattern string
+type propertyType string
 
 const (
-	headerPattern              Pattern = "headerPattern"
-	emptyLinePattern           Pattern = "emptyLinePattern"
-	newNamedMapPattern         Pattern = "newNamedMapPattern"
-	newUnnamedMapPattern       Pattern = "newUnnamedMapPattern"
-	newNamedMapSameLinePattern Pattern = "newNamedMapSameLinePattern"
-	endMapPattern              Pattern = "endMapPattern"
-	newPropPattern             Pattern = "newPropPattern"
-	undefinedPattern           Pattern = "undefinedPattern"
+	headerPattern              pattern      = "headerPattern"
+	emptyLinePattern           pattern      = "emptyLinePattern"
+	newNamedMapPattern         pattern      = "newNamedMapPattern"
+	newUnnamedMapPattern       pattern      = "newUnnamedMapPattern"
+	newNamedMapSameLinePattern pattern      = "newNamedMapSameLinePattern"
+	endMapPattern              pattern      = "endMapPattern"
+	newPropPattern             pattern      = "newPropPattern"
+	undefinedPattern           pattern      = "undefinedPattern"
+	propQuotedDate             propertyType = "propQuotedDate"
+	propUnquotedDate           propertyType = "propUnquotedDate"
+	propInteger                propertyType = "propInteger"
+	propIntArray               propertyType = "propIntArray"
+	propDecimal                propertyType = "propDecimal"
+	propDecArray               propertyType = "propDecArray"
+	propBool                   propertyType = "propBool"
+	propString                 propertyType = "propString"
 )
 
 var w string = `([\w\d\_\-\.]+)`
-var reg = map[Pattern]*regexp.Regexp{
-	headerPattern:              regexp.MustCompile(`\ACK2txt\n`),
-	emptyLinePattern:           regexp.MustCompile(`^\t*\n`),
-	newNamedMapPattern:         regexp.MustCompile(`^\t*` + w + `=\n`),
-	newNamedMapSameLinePattern: regexp.MustCompile(`^\t*` + w + `=\{\n`),
-	newUnnamedMapPattern:       regexp.MustCompile(`^\t*\{\n`),
-	endMapPattern:              regexp.MustCompile(`^\t*}\n`),
-	newPropPattern:             regexp.MustCompile(`^\t*` + w + `=([^{].*)\n`),
+var reg = map[pattern]*regexp.Regexp{
+	headerPattern:              regexp.MustCompile(`\ACK2txt\n$`),
+	emptyLinePattern:           regexp.MustCompile(`^\t*\n$`),
+	newNamedMapPattern:         regexp.MustCompile(`^\t*` + w + `=\n$`),
+	newNamedMapSameLinePattern: regexp.MustCompile(`^\t*` + w + `=\{\n$`),
+	newUnnamedMapPattern:       regexp.MustCompile(`^\t*\{\n$`),
+	endMapPattern:              regexp.MustCompile(`^\t*}\n$`),
+	newPropPattern:             regexp.MustCompile(`^\t*` + w + `=(.+)\n$`),
 }
 
 func (ck2line *CK2Line) DeterminePattern() {
@@ -43,13 +53,38 @@ func (ck2line *CK2Line) DeterminePattern() {
 			break
 		}
 	}
+	if reg[newNamedMapSameLinePattern].MatchString(ck2line.rawLine) {
+		ck2line.pattern = newNamedMapSameLinePattern
+	}
+	if reg[newUnnamedMapPattern].MatchString(ck2line.rawLine) {
+		ck2line.pattern = newUnnamedMapPattern
+	}
+}
+
+var propDate string = `(\d{3,4})\.(\d{1,2})\.(\d{1,2})`
+var propReg = map[propertyType]*regexp.Regexp{
+	propQuotedDate:   regexp.MustCompile(`^"` + propDate + `"$`),
+	propUnquotedDate: regexp.MustCompile(`^` + propDate + `$`),
+	propInteger:      regexp.MustCompile(`^([1-9]\d*)$`),
+	propIntArray:     regexp.MustCompile(`^{((?:\d+\s?)+)}$`),
+	propDecimal:      regexp.MustCompile(`^\d+\.\d+$`),
+	propDecArray:     regexp.MustCompile(`^{((?:\d+\.\d+\s?)+)}$`),
+	propBool:         regexp.MustCompile(`^yes`),
+}
+
+func (ck2line *CK2Line) DeterminePropertyType() {
+	ck2line.propertyType = propString
+	for p, r := range propReg {
+		if r.MatchString(ck2line.value) {
+			ck2line.propertyType = p
+		}
+	}
 }
 
 func NewCK2Line(l string) CK2Line {
 	ck2line := CK2Line{}
 	ck2line.rawLine = l
 	ck2line.DeterminePattern()
-
 	switch ck2line.pattern {
 	case newNamedMapPattern:
 		matches := reg[newNamedMapPattern].FindAllStringSubmatch(l, -1)[0]
@@ -60,6 +95,7 @@ func NewCK2Line(l string) CK2Line {
 	case newPropPattern:
 		matches := reg[newPropPattern].FindAllStringSubmatch(l, -1)[0]
 		ck2line.name, ck2line.value = matches[1], matches[2]
+		ck2line.DeterminePropertyType()
 	case newUnnamedMapPattern, headerPattern, endMapPattern, emptyLinePattern, undefinedPattern:
 	default:
 		panic("Expected ck2line.pattern to be defined")
